9306ee5c8ad0a3eafd04e5a24f10b9f343b261ed
Himanshu jhamb <himanshujhamb222@gmail.com>
2016-07-04 22:44:40 -0700

    Update hacker_mario.c
    just giving a styling touch

--- hacker_mario.c
+++ hacker_mario.c
@@ -7,36 +7,32 @@ int main(void)
     int height;
     do
        {
-       printf("give me a height less than 23\n");
-      height =GetInt();
+         printf("give me a height less than 23\n");
+         height =GetInt();
        }
-       while (height<0||height>=23);
+    while (height<0||height>=23);
     
     for(int i=height-1;i>=0;i--)
     {
-        for(int k=0;k<=i-1;k++)
-        {
-            printf("%c",spaces);
-        }
-        for(int j=height-1;j>=i;j--)
-        {
-            printf("%c",hashes);
-        }
-            printf("%c%c",spaces,spaces);
+         for(int k=0;k<=i-1;k++)
+          {
+             printf("%c",spaces);
+          }
+         for(int j=height-1;j>=i;j--)
+          {
+             printf("%c",hashes);
+          }
+          printf("%c%c",spaces,spaces);
         
-        for(int m=2*height+1;m>=i+height+2;m--)
-        {
+         for(int m=2*height+1;m>=i+height+2;m--)
+          {
             printf("%c",hashes);
-        }
-        for(int n=2*height+1;n>=i+height+3;n--)
-        {
+          }
+         for(int n=2*height+1;n>=i+height+3;n--)
+         {
             printf("%c",spaces);
-        } 
-    
-       
-       
-        
-    printf("\n");
-}
+         } 
+       printf("\n");
+    }
 printf("\n");
 }
