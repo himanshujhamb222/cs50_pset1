cd860e950c1ac7705505409ba506828560954c7a
himanshujhamb222 <himanshujhamb222@gmail.com>
2016-07-04 22:27:07 -0700

    Update mario.c
    just styling it

--- mario.c
+++ mario.c
@@ -6,21 +6,24 @@ int main (void)
     int row,col1,col2,height;  
     spaces=' ';
     hashes='#';
-      do
-       {
-       printf("give me a height less than 23\n");
-       height =GetInt();
-       }
-       while (height<0||height>23);
-    for(row=1; row<=height;row++){
-        for (col1=height-1;col1>=row;col1--)
+    do
+     {
+        printf("give me a height less than 23\n");
+        height =GetInt();
+     }
+     while (height<0||height>23);
+       
+         for(row=1; row<=height;row++)
         {
-        printf("%c",spaces);
-        }
+           for (col1=height-1;col1>=row;col1--)
+             {
+               printf("%c",spaces);
+             }
         
-        for(col2=1;col2<=row+1 ;col2++)
-        {printf("%c",hashes);
+           for(col2=1;col2<=row+1 ;col2++)
+            {
+              printf("%c",hashes);
+            }
+           printf("\n");
         }
-        printf("\n");
-    }
 }
